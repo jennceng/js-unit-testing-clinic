ae1ae657d11442871a34b8b991f584ac4c478bc1
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-08-29 18:13:58 -0400

    fix PersonSpec

--- spec/PersonSpec.js
+++ spec/PersonSpec.js
@@ -1,11 +1,46 @@
-describe('Book', function(){
-  describe('new Book()', function(){
-    it('takes in a title and author as arguments', function(){
-      var slaughterhouseFive = new Book('Slaughterhouse Five', 'Kurt Vonnegut');
+describe('Person', function() {
+  describe('new Person()', function() {
+    it('takes an argument for the name', function() {
+      var eben = new Person('Eben');
 
-      expect(slaughterhouseFive).toBeDefined();
-      expect(slaughterhouseFive.title).toBe('Slaughterhouse Five');
-      expect(slaughterhouseFive.author).toBe('Kurt Vonnegut');
+      expect(eben).toBeDefined();
+      expect(eben.name).toBe('Eben');
+    });
+
+    it('allows name to be an optional argument which defaults to Jane Doe', function() {
+      var anonymous = new Person();
+
+      expect(anonymous).toBeDefined();
+      expect(anonymous.name).toBe('Jane Doe');
+    });
+  });
+
+  describe('greet()', function() {
+    it('returns a string with a greeting that includes the persons name', function() {
+      var nick = new Person('Nick');
+
+      expect(nick.greet()).toBe('Nick says hello!');
+    });
+  });
+
+  describe('new Person() creates an object that is an instance of Person constructor', function() {
+    it('returns true when checking instance prototype', function() {
+      var chelsea = new Person("Chelsea");
+      var jesse = new Person("Jesse");
+
+      expect(chelsea instanceof Person).toBe(true);
+    });
+
+    it('returns false when using the object literal with the same properties', function(){
+      var jesse = {
+        name: "Jesse",
+        greet: function(){
+          return this.name + " says hello!";
+        }
+      };
+
+      expect(jesse instanceof Person).toBe(false);
     })
   })
-})
+
+});
